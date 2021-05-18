e66c8a77374e2b7d0fa5b25e396659f5bd03e584
Adam Mann <aemann2@gmail.com>
2021-05-17 21:25:40 -0700

    finished styling

--- main.js
+++ main.js
@@ -13,21 +13,51 @@ function validateEmail(email) {
 
 const handleSubmit = (e) => {
   e.preventDefault();
+
   const fnameErr = document.querySelector('.error__fname');
-  const fnameErr = document.querySelector('.error__fname');
-  const fnameErr = document.querySelector('.error__fname');
-  const fnameErr = document.querySelector('.error__fname');
+  const lnameErr = document.querySelector('.error__lname');
+  const emailErr = document.querySelector('.error__email');
+  const pwordErr = document.querySelector('.error__pword');
 
   if (fname.value === '') {
     fnameErr.style.display = 'block';
+    fname.style.background =
+      'url("../images/icon-error.svg") no-repeat 95% 50%';
+    fname.style.borderColor = 'hsl(0, 100%, 74%)';
   } else {
     fnameErr.style.display = 'none';
+    fname.style.background = 'none';
+    fname.style.borderColor = 'hsl(246, 25%, 77%)';
   }
   if (lname.value === '') {
+    lnameErr.style.display = 'block';
+    lname.style.background =
+      'url("../images/icon-error.svg") no-repeat 95% 50%';
+    lname.style.borderColor = 'hsl(0, 100%, 74%)';
+  } else {
+    lnameErr.style.display = 'none';
+    lname.style.background = 'none';
+    lname.style.borderColor = 'hsl(246, 25%, 77%)';
   }
   if (!validateEmail(email.value)) {
+    emailErr.style.display = 'block';
+    email.style.background =
+      'url("../images/icon-error.svg") no-repeat 95% 50%';
+    email.style.borderColor = 'hsl(0, 100%, 74%)';
+  } else {
+    emailErr.style.display = 'none';
+    email.style.background = 'none';
+    email.style.borderColor = 'hsl(246, 25%, 77%)';
   }
-  if (password.value.length < 6) {
+  if (password.value.length < 1) {
+    pwordErr.style.display = 'block';
+    password.style.background =
+      'url("../images/icon-error.svg") no-repeat 95% 50%';
+    password.style.borderColor = 'hsl(0, 100%, 74%)';
+  } else {
+    pwordErr.style.display = 'none';
+    password.style.background = 'none';
+    password.style.borderColor = 'hsl(246, 25%, 77%)';
   }
 };
 
